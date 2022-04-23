d7e187c4e4c92a72704c5224301c8c61427abd69
Mido Ryu <101682300+midotype@users.noreply.github.com>
2022-04-24 00:12:49 +0900

    Time: 100 ms (44.54%), Space: 45.6 MB (84.71%) - LeetHub

---
+++ 704-binary-search/704-binary-search.ts
@@ -0,0 +1,13 @@
+function search(nums: number[], target: number): number {
+	let low: number = 0;
+	let high: number = nums.length-1;
+	
+	while(low <= high) {
+		let mid: number = low + ((high - low) >> 1);
+		if(nums[mid] == target) return mid;
+		else if(nums[mid] < target) low = mid + 1;
+		else high = mid - 1;
+	}
+
+	return -1;
+};
