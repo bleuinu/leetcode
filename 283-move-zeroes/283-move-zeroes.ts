dd87db4b714aaabb82f376c0e6ce1123fc483fa4
Mido Ryu <101682300+midotype@users.noreply.github.com>
2022-04-26 08:57:52 +0900

    Time: 112 ms (74.85%), Space: 47.6 MB (39.61%) - LeetHub

--- 283-move-zeroes/283-move-zeroes.ts
+++ 283-move-zeroes/283-move-zeroes.ts
@@ -5,7 +5,7 @@ function moveZeroes(nums: number[]): void {
     let pos: number = 0;
     if(nums.includes(0)) {
         nums.forEach((part, idx, nums) => {
-            if(nums[idx] != 0) {
+            if(nums[idx] !== 0) {
                 let t: number = nums[pos];
                 nums[pos] = nums[idx];
                 nums[idx] = t;
