7d701585ffe8b9a7c2709cecd1803027c0bb6ba1
Mido Ryu <101682300+midotype@users.noreply.github.com>
2022-04-27 06:16:58 +0900

    Time: 104 ms (57.31%), Space: 49.8 MB (29.53%) - LeetHub

--- 557-reverse-words-in-a-string-iii/557-reverse-words-in-a-string-iii.ts
+++ 557-reverse-words-in-a-string-iii/557-reverse-words-in-a-string-iii.ts
@@ -1,21 +1,26 @@
-function reverse(s: string): string {
-    let s2: string[]= s.split('');
-    const sz: number = s2.length;
-    const bound: number = sz >> 1;
-    for(let i=0; i<bound; ++i) {
-        let c: string = s2[i];
-        s2[i] = s2[sz-i-1];
-        s2[sz-i-1] = c;
+function reverseWords(s: string): string {
+    let begin: number = 0;
+    let end: number = 0;
+    let sa: string[] = s.split('');
+    while(true) {
+        if(sa[end] === undefined || sa[end] === ' ') {
+            if(end != begin) {
+                let i:number = begin;
+                let j:number = end-1;
+                while(i<j) {
+                    let c: string = sa[i];
+                    sa[i] = sa[j];
+                    sa[j] = c;
+                    ++i;
+                    --j;
+                }
+            }
+            begin = end + 1;
+        }
+        
+        if(sa[end] === undefined) break;
+        ++end;
     }
     
-    return s2.join('');
-}
-function reverseWords(s: string): string {
-    let arr: string[] = s.split(' ');
-    let ans: string[] = [];
-    arr.forEach((word) => {
-        ans.push(reverse(word));
-    },this)
-    
-    return ans.join(' ');
+    return sa.join('');
 };
