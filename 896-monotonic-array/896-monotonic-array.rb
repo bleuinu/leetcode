e788c2d09237d280e04fa58c80d81f641ac1ab8b
Mido Eu <101682300+midotype@users.noreply.github.com>
2022-05-03 23:28:58 +0900

    Time: 513 ms (7.41%), Space: 241 MB (7.41%) - LeetHub

--- 896-monotonic-array/896-monotonic-array.rb
+++ 896-monotonic-array/896-monotonic-array.rb
@@ -1,18 +1,5 @@
 # @param {Integer[]} nums
 # @return {Boolean}
 def is_monotonic(nums)
-    store = 0
-    ((nums.size)-1).times do |i|
-        c = nums[i] - nums[i+1];
-        c = (c < 0) ? -1 : (c > 0) ? 1 : 0;
-        
-        if c != 0
-            if c != store and store != 0
-                return false
-            end
-            store = c
-        end
-    end
-    
-    return true
+    return nums.sort == nums || nums.sort.reverse == nums
 end
