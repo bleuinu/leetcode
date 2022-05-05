7185949b41a4b7c6309b11215f4c2dd5dbdb9c9c
Mido Eu <101682300+midotype@users.noreply.github.com>
2022-05-05 14:38:21 +0900

    Time: 12 ms (14.04%), Space: 7.9 MB (14.65%) - LeetHub

--- 1672-richest-customer-wealth/1672-richest-customer-wealth.cpp
+++ 1672-richest-customer-wealth/1672-richest-customer-wealth.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int maximumWealth(vector<vector<int>>& accounts) {
         int m = -1;
-        for(vector<int> account: accounts) {
+        for(vector<int> &account: accounts) {
             int sum = accumulate(account.begin(), account.end(), 0);
             m = max(sum, m);
         }
